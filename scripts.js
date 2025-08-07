7b56a80d14ad55637c5f4ed6aa9bc2e9c824e05d
noskillcl <154913828+noskillcl@users.noreply.github.com>
2025-08-07 10:39:02 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Función para mostrar un mensaje extra cuando se hace clic
+function showLove() {
+    const extraMessage = document.getElementById('extraMessage');
+    extraMessage.classList.remove('hidden'); // Mostrar el mensaje extra
+}
